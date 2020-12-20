640b548e987263f49d4cf2d87598d30e60680428
abelt <andrey.beltiukov@piano.io>
2020-12-20 01:28:15 +0000

    Remove unused

--- src/main/java/mystrategy/maps/EnemiesMap.java
+++ src/main/java/mystrategy/maps/EnemiesMap.java
@@ -4,35 +4,27 @@ import model.Coordinate;
 import model.Entity;
 import model.EntityProperties;
 import model.PlayerView;
-import mystrategy.Constants;
 import mystrategy.collections.AllEntities;
 import util.DebugInterface;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class EnemiesMap {
     private int[][] shootDangerNextTick;
-    private int[][] distanceByFoot;
     private int[][] shootDanger;
-    private EntitiesMap entitiesMap;
     private int mapSize;
 
     public EnemiesMap(PlayerView playerView, EntitiesMap entitiesMap, AllEntities entities) {
-        this.entitiesMap = entitiesMap;
         mapSize = playerView.getMapSize();
-        distanceByFoot = new int[mapSize][mapSize];
         shootDanger = new int[mapSize][mapSize];
 
-        Set<Coordinate> enemyCoordinates = new HashSet<>(128);
         Set<Coordinate> shoot5Coordinates = new HashSet<>(128);
         Set<Coordinate> shoot1Coordinates = new HashSet<>(128);
         for (int i = 0; i < mapSize; i++) {
             for (int j = 0; j < mapSize; j++) {
                 if (entitiesMap.getIsEnemy(i, j)) {
                     Coordinate position = new Coordinate(i, j);
-                    enemyCoordinates.add(position);
 
                     EntityProperties properties = entitiesMap.getEntity(i, j).getProperties();
                     if (properties.getAttack() != null
@@ -47,7 +39,6 @@ public class EnemiesMap {
             }
         }
 
-        fillDistances(distanceByFoot, enemyCoordinates);
         fillShootDanger(shootDanger, shoot5Coordinates, shoot1Coordinates);
 /*
         List<Coordinate> coordinates = Arrays.stream(playerView.getEntities()).filter(ent -> ent.getPlayerId() != playerView.getMyId())
@@ -126,17 +117,6 @@ public class EnemiesMap {
     }
 
 
-    public int getDistance(Coordinate position) {
-        return getDistance(position.getX(), position.getY());
-    }
-
-    public int getDistance(int x, int y) {
-        if (x < 0 || y < 0 || x >= mapSize || y >= mapSize) {
-            return 0;
-        }
-        return distanceByFoot[x][y];
-    }
-
     private void fillShootDanger(
             int[][] dangerMap, Set<Coordinate> coordinateList5,
             Set<Coordinate> coordinateList1
@@ -171,89 +151,11 @@ public class EnemiesMap {
 //        }
     }
 
-    private void fillDistances(int[][] distanceMap, Set<Coordinate> coordinateList) {
-        for (int i = 1; !coordinateList.isEmpty(); i++) {
-            Set<Coordinate> coordinateListNext = new HashSet<>(128);
-            for (Coordinate coordinate : coordinateList) {
-                if (coordinate.getX() >= 0 && coordinate.getX() < mapSize
-                        && coordinate.getY() >= 0 && coordinate.getY() < mapSize
-                        && getDistance(coordinate) == 0
-                        && isPassable(coordinate)) {
-                    distanceMap[coordinate.getX()][coordinate.getY()] = i;
-                    coordinateListNext.add(new Coordinate(coordinate.getX() - 1, coordinate.getY() + 0));
-                    coordinateListNext.add(new Coordinate(coordinate.getX() + 0, coordinate.getY() + 1));
-                    coordinateListNext.add(new Coordinate(coordinate.getX() + 0, coordinate.getY() - 1));
-                    coordinateListNext.add(new Coordinate(coordinate.getX() + 1, coordinate.getY() + 0));
-                }
-            }
-            coordinateList = coordinateListNext;
-
-            if (i > Constants.MAX_CYCLES) {
-                if (DebugInterface.isDebugEnabled()) {
-                    throw new RuntimeException("protection from endless cycles");
-                } else {
-                    break;
-                }
-            }
-        }
-
-/*
-        for (int i = 0; i < mapSize; i++) {
-            for (int j = 0; j < mapSize; j++) {
-                if (DebugInterface.isDebugEnabled()) {
-                    DebugCommand.Add command = new DebugCommand.Add();
-                    ColoredVertex coloredVertex = new ColoredVertex(new Vec2Float(i, j), new Vec2Float(0, 0), new Color(0, 0, 0, 0.5f));
-                    DebugData data = new DebugData.PlacedText(coloredVertex, Integer.toString(distanceByFoot[i][j]), -1, 12);
-                    command.setData(data);
-                    debugInterface.send(command);
-                }
-            }
-        }
-*/
-
-    }
-
-    private boolean isPassable(Coordinate coordinate) {
-        return this.entitiesMap.isPassable(coordinate) || this.entitiesMap.getIsEnemy(coordinate);
-    }
-
-    public Coordinate getMinOfTwoPositions(Coordinate old, Coordinate newPosition) {
-        if (newPosition.getX() < 0 || newPosition.getY() < 0 || newPosition.getX() >= mapSize || newPosition.getY() >= mapSize) {
-            return old;
-        }
-        if (getDistance(newPosition) == 0) {
-            return old;
-        }
-        if (getDistance(newPosition) < getDistance(old)) {
-            return newPosition;
-        }
-        return old;
-    }
-
-    public Coordinate getPositionClosestToEnemy(Coordinate from) {
-//        return getPositionClosestToEnemy(from, from.getAdjacentList());
-        int radius = 4;
-        for (int i = -radius; i <= radius; i++) {
-            for (int j = -radius; j <= radius; j++) {
-                from = getMinOfTwoPositions(from, new Coordinate(from.getX() + i, from.getY() + j));
-            }
-        }
-        return from;
-    }
-
     public int getDangerLevel(Coordinate from) {
         if (from.isOutOfBounds()) {
             return 0;
         }
         return shootDanger[from.getX()][from.getY()];
-    }
-
-    public Coordinate getPositionClosestToEnemy(Coordinate from, List<Coordinate> coordinateList) {
-        Coordinate position = from;
-        for (Coordinate newPosition : coordinateList) {
-            position = getMinOfTwoPositions(position, newPosition);
-        }
-        return position;
     }
 
     public int getDamageOnNextTick(Coordinate newPosition) {
