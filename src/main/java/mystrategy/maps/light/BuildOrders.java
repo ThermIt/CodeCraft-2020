4a0359639441b5bf87bd67e4ce2805211b869490
abelt <andrey.beltiukov@piano.io>
2021-01-04 16:59:53 +0000

    Sandbox version 2

--- src/main/java/mystrategy/maps/light/BuildOrders.java
+++ src/main/java/mystrategy/maps/light/BuildOrders.java
@@ -3,7 +3,6 @@ package mystrategy.maps.light;
 import model.*;
 import mystrategy.collections.AllEntities;
 import mystrategy.maps.EntitiesMap;
-import util.DebugInterface;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -42,14 +41,16 @@ public class BuildOrders {
             buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(5, 2), 0, false));
             buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(2, 5), 0, false));
         } else {
-/*
             buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(2, 2), 0, false));
             buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(5, 2), 0, false));
             buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(2, 5), 0, false));
             buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(8, 2), 0, false));
             buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(2, 8), 0, false));
-*/
+//            buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(11, 2), 0, false));
+//            buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(2, 11), 0, false));
 
+
+/*
             int o=0;
             for (int i = 0; i < 10; i++) {
                 for (int j = 0; j <= i; j++) {
@@ -57,16 +58,8 @@ public class BuildOrders {
                     DebugInterface.println("X"+o++, j*4, (i-j)*4,2);
                 }
             }
-        }
-/*
-        for (int i = 0; i < 5; i++) {
-            buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(11+i*3, 0), 0, false));
-            buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(0, 12+i*3), 0, false));
-
-//            buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(11+i*3, 4), 0, false));
-//            buildQueue.add(new Entity(-1, getMyId(), EntityType.HOUSE, new Coordinate(4, 12+i*3), 0, false));
-        }
 */
+        }
 /*
         buildQueue.add(new Entity(-1, getMyId(), EntityType.TURRET, new Coordinate(10, 5), 0, false));
         buildQueue.add(new Entity(-1, getMyId(), EntityType.TURRET, new Coordinate(5, 10), 0, false));
@@ -97,8 +90,8 @@ public class BuildOrders {
     }
 
     public boolean isFreeToAdd() {
-        return /*(entities.getMyHouses().size() >= 4 && entities.getMyHouses().size() <= 8)
-                || */(entities.getMyRangedBases().size() > 0 && entities.getMyRangedBases().get(0).getHealth() > 10);
+        return (entities.getMyHouses().size() >= 4 && entities.getMyHouses().size() <= 8)
+                || (entities.getMyRangedBases().size() > 0 && entities.getMyRangedBases().get(0).getHealth() > 10);
     }
 
     public List<Entity> updateAndGetActiveOrders(AllEntities allEntities, EntitiesMap entitiesMap, Player me) {
